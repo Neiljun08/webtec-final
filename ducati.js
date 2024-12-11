a6da2a00dbc4161a67b0b3b5824108fb15cdb967
Neiljun08 <ubodneiljun914@gmail.com>
2024-12-11 18:26:52 +0800

    Add files via upload

---
+++ ducati.js
@@ -0,0 +1,28 @@
+// Fetch the JSON data
+fetch("product.json")
+    .then((response) => response.json())
+    .then((data) => {
+        const productList = document.getElementById("product-list");
+
+        // Filter products with IDs 6 to 10
+        const filteredProducts = data.filter(product => product.id >= 1 && product.id <= 3);
+
+        // Render the filtered products
+        filteredProducts.forEach(product => {
+            const productItem = document.createElement("div");
+            productItem.classList.add("product1");
+            productItem.innerHTML = `
+                <img src="${product.image}" alt="${product.title}">
+                <div>
+                    <h2>${product.title}</h2>
+                    <p>${product.description}</p>
+                    <p class="price">Price: ₱${product.Price.toFixed(2)}</p>
+                </div>
+            `;
+            productList.appendChild(productItem);
+        });
+    })
+    
+    .catch((error) => {
+        console.error("Error fetching the product data:", error);
+    });
